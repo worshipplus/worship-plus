ddb08c54d33c089771d85940cf6cdd16ab088c61
Matheus Francisco da Silva Lima Gomes <matheus.lima.gomes@outlook.com>
2026-03-04 10:15:17 -0300

    chore(lighthouse): increase server ready timeout [US-013]

--- frontend/lighthouse.config.cjs
+++ frontend/lighthouse.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       startServerCommand:
-      'npm --prefix frontend run preview -- --strictPort --port 4173',
+        'npm --prefix frontend run preview -- --strictPort --port 4173',
       startServerReadyPattern: 'Local:',
       startServerReadyTimeout: 120000,
       url: ['http://127.0.0.1:4173/'],
